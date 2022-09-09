a9655ce0e143208293c786d861aca3fbbaf747ae
EndlessMilkyway <freekyr7529@gmail.com>
2022-09-10 03:18:55 +0900

    2022.09.10 정신차리자

--- src/main/java/com/milkyway/study/service/posts/PostsService.java
+++ src/main/java/com/milkyway/study/service/posts/PostsService.java
@@ -28,7 +28,6 @@ public class PostsService {
 
         return id;
     }
-
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
--- src/main/java/com/milkyway/study/web/PostsApiController.java
+++ src/main/java/com/milkyway/study/web/PostsApiController.java
@@ -7,6 +7,7 @@ import com.milkyway.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
